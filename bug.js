94f18b71dd6c5481a8762eef97bbd2c4e94d43e8
BugHunter-X <askzenai@gmail.com>
2025-02-20 12:28:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo CLI to build an Android app.  The build process fails with a cryptic error message related to Android resource linking or similar. The error message itself is not very helpful in pinpointing the exact cause.
